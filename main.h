ce26c92db3c524a4ac6253b6d5eb7c98b533ccd7
xov121 <x.o.v121@gmail.com>
2023-11-26 17:37:27 -0400

    Added NULL definition to main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#ifndef NULL
+#define NULL ((void *)0)
+#endif //NULL
+
+int _printf(const char *format, ...);
+int print_char(char c);
+int print_string(char *s);
+int print_percent(void);
+int _strlen(char *s);
+
+#endif //MAIN_H
