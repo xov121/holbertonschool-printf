35dd0f5db7ef1d5698fa634a624a8376aa2959e7
DilanSoto <144055652+DilanSoto@users.noreply.github.com>
2023-11-27 00:01:07 -0400

    Update main.h

--- main.h
+++ main.h
@@ -1,21 +1,31 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
 
-#ifndef NULL
-#define NULL ((void *)0)
-#endif /* NULL */
 
-#include <limits.h> // For INT_MIN macro in print_int
-#include <stdarg.h> // For va
-#include <unistd.h> // For write
-#include <stdio.h> // For printf
-
-int _putchar(char c);
-int _strlen(char *s);
-int _printf(const char *format, ...);
-int print_char(va_list ap);
-int print_string(va_list ap);
-int print_percent(va_list ap);
-int print_int(va_list ap);
-
-#endif /* MAIN_H */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int len;
+    int len2;
+    
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+    len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
+    _printf("Len:[%d]\n", len);
+    printf("Len:[%d]\n", len2);
+    return (0);
+}
