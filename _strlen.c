79621add433bd310369d6355a29b400eff217170
DilanSoto <144055652+DilanSoto@users.noreply.github.com>
2023-11-26 23:49:44 -0400

    Create _strlen.c

---
+++ _strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - Function that counts the lenght of a string.
+ * @s: The string to be counted.
+ *
+ * Return: The lenght of the string.
+ */
+int _strlen(char *s)
+{
+    int i;
+    for (i = 0; s[i] != '\0'; i++)
+        {
+        }
+    return (i);
+}
