cc2a18c5fdb93790e5368e77cbba57506c50dbbd
DilanSoto <144055652+DilanSoto@users.noreply.github.com>
2023-11-27 00:09:14 -0400

    Update main.h

--- main.h
+++ main.h
@@ -1,29 +1,26 @@
-#include "main.h" 
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+int print_character(va_list arg);
+int print_string(va_list arg);
+int print_percent(va_list arg);
+int _printf(const char *format, ...);
+int print_integers(va_list arg);
+int default_case(const char *format);
 
 /**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
+ *struct printf - struct for printf
+ * @print: function pointer
+ * @charType: specifier
+*/
+
+typedef struct printf
 {
-    int len;
-    int len2;
-    
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
-    _printf("Len:[%d]\n", len);
-    printf("Len:[%d]\n", len2);
-    return (0);
-}
+char *charType;
+int (*print)(va_list arg);
+} printf_t;
+
+
+#endif
