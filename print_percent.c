d433142b7d6749c6fc96e7c34ec0949062bec2c3
xov121 <x.o.v121@gmail.com>
2023-11-27 03:57:43 -0400

    WIP

--- print_percent.c
+++ print_percent.c
@@ -6,14 +6,7 @@
  * 
  * Return: Number of characters printed, 1 for success.
  */
-<<<<<<< HEAD
 int print_percent(va_list ap){
     (void)ap;
     return (_putchar('%'));
-=======
-int print_percent(va_list ap)
-{
-(void)ap;
-return (_putchar('%'));
->>>>>>> dc57e312d1cdf5480dea926e1daede2086405297
 }
