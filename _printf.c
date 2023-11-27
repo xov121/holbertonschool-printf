c951c69d6cd3a8028a20121b1f4ba3135ef0685b
DilanSoto <144055652+DilanSoto@users.noreply.github.com>
2023-11-26 23:48:33 -0400

    Create _printf.c

---
+++ _printf.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _printf - Custom implementation of the C library function printf.
+ * @format: The format string that contains the text to be written to stdout.
+ *          It may contain embedded format specifiers that are replaced by
+ *          the values specified in subsequest additional arguments.
+ * 
+ * Return: Numbers of characters printed (excluding the NULL byte at the end of the string
+ * or -1 if there is an error.
+ */
+int _printf(const char *format, ...){
+    int i, j;
+    int char_count = 0;
+    va_list ap;
+
+    print_type argument[] = {
+        {"c", print_char},
+        {"s", print_string},
+        {"%", print_percent},
+        {"d", print_int},
+        {"i", print_int},
+        {NULL, NULL}
+    };
+    
+    va_start(ap, format);
+    if (!format || (format[0] == '%' && !format[1]))
+     return (-1);
+
+    for (i = 0; format && format[i]; i++){
+        if (format[i] != '%'){
+            char_count += _putchar(format[i]);
+            continue;
+        }
+        for (j = 0; argument[j].parameter; j++){
+            if (*argument[j].parameter == format [i + 1]){
+                char_count += argument[j].f(ap);
+                break;
+            }
+        }
+        i++;
+        if (!argument[j].parameter){
+            char_count += _putchar('%');
+            char_count += _putchar(format[i + 1], 1);
+        }
+    }
+    va_end(ap);
+    return (char_count);
+}
